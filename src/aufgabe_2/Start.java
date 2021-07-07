af57f221c171ba62dc1f8beb7f6291a9717d26ea
Benedikt Kurz <bekur002@stud.fh-dortmund.de>
2021-07-07 17:16:19 +0200

    Main implemented

--- src/aufgabe_2/Start.java
+++ src/aufgabe_2/Start.java
@@ -1,9 +1,30 @@
 package aufgabe_2;
 
+import java.io.IOException;
+import java.io.StringReader;
+
 public class Start {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		
+		// Starting the parser
+		RegAusdruck parser = new RegAusdruck(new StringReader("(ab|c*"));
+		
+		try {
+			
+			parser.cu();
+			
+		} catch (ParseException e) {
+			
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("An error occured during parsing");
+			
+		}
+		
+		System.out.println("running succesfull");
+		
+		
 
 	}
 
