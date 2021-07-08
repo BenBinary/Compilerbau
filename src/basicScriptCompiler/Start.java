3c1598a6ccb0fa3234aa029bc5aff47143bdc8ca
Benedikt Kurz <bekur002@stud.fh-dortmund.de>
2021-07-08 12:32:01 +0200

    Input stream works now fine

--- src/basicScriptCompiler/Start.java
+++ src/basicScriptCompiler/Start.java
@@ -1,8 +1,34 @@
 package basicScriptCompiler;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class Start {
 	
 	public static void main(String[] args) {
+		
+		File file = new File("src/hello.scrt");
+		
+		
+		try {
+			
+			
+			FileInputStream fis = new FileInputStream(file);
+			BasicScript parser = new BasicScript(fis);
+			
+			parser.cu();
+			
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 		
 	}
 
